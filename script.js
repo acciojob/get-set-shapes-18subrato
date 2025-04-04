2dd15218b3cbfe9b260b90b3c1b7f6005de4d774
Subrato Kumar Verma <138965935+18subrato@users.noreply.github.com>
2025-04-04 23:13:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,41 +1,32 @@
 class Rectangle {
-	private _width: number;
-  private _height: number;
+	constructor(width,height){
+		this.width = width;
+		this.heigth = height;
+	}
 
-  constructor(width: number, height: number) {
-    if (width <= 0 || height <= 0) {
-      throw new Error("Width and height must be positive integers.");
-    }
-    this._width = width;
-    this._height = height;
-  }
+	get width(){
+		return this.width;
+	}
 
-  get width(): number {
-    return this._width;
-  }
+	get height(){
+		return this.height;
+	}
 
-  get height(): number {
-    return this._height;
-  }
-
-  getArea(): number {
-    return this._width * this._height;
-  }
+	getArea(){
+		return this.width*this.height;
+	}
+	
 }
  
- class Square extends Animal {
-	 constructor(side: number) {
-    if (side <= 0) {
-      throw new Error("Side must be a positive integer.");
-    }
-    super(side, side);
-  }
+class Square extends Animal {
+	constructor(side){
+		this.side = side;
+	}
+	getPerimeter(){
+		return 4*this.side;
+	}
+}
 
-  getPerimeter(): number {
-    return 4 * this.width;
-  }
- }
- 
  // Do not change the code below this line
  window.Rectangle = Rectangle;
  window.Square = Square;
